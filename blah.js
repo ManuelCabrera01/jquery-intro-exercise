bb1bcd965dfe292f907dcea9870d351c13ef6b6e
Nicholas Borbe <nicholasborbe@Nicholass-MacBook-Pro-2.local>
2017-07-13 14:54:02 -0400

    solution added

---
+++ blah.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+$("#menu > ul >li:first").addClass("selected");
+$(".container > article:nth-child(2)").addClass("middle");
+$(".list-item:even").addClass("highlighted");
+$(".list-item:last").css("border", "2px solid black")
+$("input:focus").css("border", "5px solid black")
+
+
+
+});
